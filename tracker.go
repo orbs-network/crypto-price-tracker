5be30edcd75003729a87c58f8b6e1cc0bab7af56
Leonid Beder <leonid.beder@gmail.com>
2018-06-15 23:36:04 +0300

    Add initial app

---
+++ tracker.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/urfave/cli"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "Cryptocurrencies Price Tracker"
+	app.Usage = "track the last 15 days cryptocurrency's average price using CMC historic data web page scraper"
+
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "date",
+			Value: "now",
+			Usage: `the price calculating date (e.g., "2018-06-15"`,
+		},
+	}
+
+	app.Action = func(c *cli.Context) error {
+		var targetTime time.Time
+		date := c.String("date")
+		if date == "now" {
+			targetTime = time.Now().Local()
+		} else {
+			var err error
+			targetTime, err = time.Parse("2006-01-02", date)
+			if err != nil {
+				return err
+			}
+		}
+		fmt.Println("Target time is", targetTime.Format("2006-01-02"))
+
+		return nil
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
