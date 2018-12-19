28a1593d56e59a832e83a3f5d80e5ca6c1399197
Leonid Beder <leonid.beder@gmail.com>
2018-12-19 23:40:04 +0200

    Install dependencies

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
 #!/bin/bash -e
 
+go get github.com/urfave/cli
+go get github.com/PuerkitoBio/goquery
+go get github.com/RobinUS2/golang-moving-average
+go get github.com/kardianos/osext
+go get github.com/tealeg/xlsx
+
 go build -o bin/crypto-price-tracker
