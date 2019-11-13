5698a362055044abe8f330bffe1488d2a82dce4f
Leonid Beder <leonid.beder@gmail.com>
2019-11-13 18:37:18 +0200

    Build without symbol and debug info

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ go get github.com/RobinUS2/golang-moving-average
 go get github.com/kardianos/osext
 go get github.com/tealeg/xlsx
 
-go build -o bin/crypto-price-tracker
+go build -ldflags="-s -w" -o bin/crypto-price-tracker
