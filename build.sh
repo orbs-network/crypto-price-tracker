f7d06aa1b77f7d2d8ee29c3e289d7b8f7d1d24eb
Degenddy <degenddy@users.noreply.github.com>
2021-09-14 11:42:57 +0300

    minor fix to allow double click running

--- build.sh
+++ build.sh
@@ -7,7 +7,8 @@ go get github.com/tealeg/xlsx
 
 go build -ldflags="-s -w" -o bin/crypto-price-tracker
 
-echo './crypto-price-tracker --priorityUsername david --priorityPassword 1234 --priorityEndpoint "https://sandbox.priority-software.com/sbx5/odata/Priority/tabp3a1a.ini/cockpit"' > ./bin/run_priority_test.sh
-chmod +x ./bin/run_priority_test.sh
+echo 'cd -- "$(dirname -- "$BASH_SOURCE")"' > ./bin/run_priority_test.sh
+echo './crypto-price-tracker --priorityUsername david --priorityPassword 1234 --priorityEndpoint "https://sandbox.priority-software.com/sbx5/odata/Priority/tabp3a1a.ini/cockpit"' >> ./bin/run_priority_test.sh
+chmod a+x ./bin/run_priority_test.sh
 
 
