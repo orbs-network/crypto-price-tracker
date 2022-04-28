f1dbf734d453b45a7a741123ffab3db5b32d705e
Degenddy <degenddy@users.noreply.github.com>
2022-04-28 11:35:36 +0300

    minor fix

--- build.sh
+++ build.sh
@@ -9,6 +9,7 @@ go build -ldflags="-s -w" -o bin/crypto-price-tracker
 
 echo 'cd -- "$(dirname -- "$BASH_SOURCE")"' > ./bin/run_priority_test.sh
 echo './crypto-price-tracker --priorityUsername david --priorityPassword 1234 --priorityEndpoint "https://sandbox.priority-software.com/sbx5/odata/Priority/tabp3a1a.ini/cockpit"' >> ./bin/run_priority_test.sh
+chmod a+x ./bin/crypto-price-tracker
 chmod a+x ./bin/run_priority_test.sh
 
 
